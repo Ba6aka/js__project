16a7ff0fb4c5afd7b034686b4d3286cce79402e5
Ba6aka <bam6ykba6aka@gmail.com>
2022-11-19 16:20:26 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// let introduce = 'Hello, user, my name Andrii';
+// alert(introduce);
+
+// let answer = 'do you speak English?';
+// console.log( confirm(answer));
+
+// console.log(prompt ('How old are you?', ''));
+// console.log(prompt ('do you like me?', ''));
+
+// let str1 = '22',
+//     str2 = 8;
+
+// console.log(str1 + str2 + 'mothefucker');
+
+// let incr = 11,
+//     dcr = 11;
+
+
+// console.log(++incr)
+// console.log(--dcr);
+
+// console.log(2 * 2 === '4');
+
+// let op1 = true,
+//     op2 = false;
+
+// console.log(op1 || op2)
+
+const obgOne = {
+    camry: 11,
+    nissan: 12
+}
+
+const car = 'lexus';
+obgOne[car] = 13.5 + obgOne['camry'];
+console.log(obgOne)
